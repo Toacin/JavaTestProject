ecf7ba22743c819a595c4d501307a95e9d763ab2
Toacin <toacinp@outlook.com>
2022-12-24 00:58:39 -0700

    "initialized packages"

--- src/main/main.java
+++ src/main/main.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.sql.SQLOutput;
+
 public class main {
     public static void main(String[] args) {
+        int testNum = 4;
+        testNum = -5;
+        System.out.println(testNum);
 
     }
 }
