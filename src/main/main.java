ea5ae3dd97328c3efd22a07f1c7dec45fa0f6526
Toacin <toacinp@outlook.com>
2022-12-26 14:45:34 -0700

    changes to initialization complete

--- src/main/main.java
+++ src/main/main.java
@@ -4,9 +4,25 @@ import java.sql.SQLOutput;
 
 public class main {
     public static void main(String[] args) {
-        int testNum = 4;
-        testNum = -5;
-        System.out.println(testNum);
-
+        boolean whileCondition = true;
+        int count = 0;
+        int primeCount = 0;
+        while (whileCondition) {
+            boolean isPrime = true;
+            for (int i=2; i<count; i++) {
+                if (count % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeCount++;
+            }
+            if (primeCount == 100) {
+                System.out.println(count);
+                whileCondition = false;
+            }
+            count++;
+        }
     }
 }
